1d208278518b0d8391c4e3d80d5358eb1910cc66
徐小荣 <xiaorong.xu@bigtimes.io>
2022-05-30 22:54:56 +0800

    Connect to mysql with fixed parameters.

---
+++ connect.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * \file
+ * \brief DB variables for EasyPHP.
+ * 
+ * @author https://sourceforge.net/projects/lwt/ LWT Project
+ * @since  1.0.3
+ */
+
+$server = "lwt_db";
+$userid = "root";
+$passwd = "abcxyz";
+$dbname = "learning-with-texts";
+
+?>
